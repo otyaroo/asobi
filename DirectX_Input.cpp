85170effa8970fec96b3fd4663b4d90b36df3f91
ryo <otyaroo@live.jp>
2015-10-04 10:25:36 +0900

    キー押下時にディスプレイに表示するキーの追加
    新たにカーソルキーとCキーを追加

--- DirectX_Input.cpp
+++ DirectX_Input.cpp
@@ -8,4 +8,25 @@ void KeyShow(char* Keyinput) {
 	if (Keyinput[KEY_INPUT_X] == 1) {
 		DrawFormatString(0, 20, WhiteColor, "Xキーが押されました。");
 	}
+	if (Keyinput[KEY_INPUT_C] == 1) {
+		DrawFormatString(0, 40, WhiteColor, "Cキーが押されました。");
+	}
+	if (Keyinput[KEY_INPUT_LEFT] == 1) {
+		DrawFormatString(0, 60, WhiteColor, "←キーが押されました。");
+	}
+	if (Keyinput[KEY_INPUT_RIGHT] == 1) {
+		DrawFormatString(0, 80, WhiteColor, "→キーが押されました。");
+	}
+	if (Keyinput[KEY_INPUT_UP] == 1) {
+		DrawFormatString(0, 100, WhiteColor, "↑キーが押されました。");
+	}
+	if (Keyinput[KEY_INPUT_DOWN] == 1) {
+		DrawFormatString(0, 120, WhiteColor, "↓キーが押されました。");
+	}
+	if (Keyinput[KEY_INPUT_SPACE] == 1) {
+		DrawFormatString(0, 140, WhiteColor, "SPACEキーが押されました。");
+	}
 }
+
+//方向キーは３つ同時押しには対応してない。MacbookAir 2012の仕様かもしれないけど。
+//３つ同時に押して操作することはないだろうが一応。

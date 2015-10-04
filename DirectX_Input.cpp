c478223a4a05c9d7be25066ee6a25821ebdb5daf
ryo <otyaroo@live.jp>
2015-10-04 10:50:57 +0900

    MBA上での制約なのか？
    １，２キーを押しながらZキーは押せない
    ZXCを押しながらA,Sキーは押せない。
    んー？

--- DirectX_Input.cpp
+++ DirectX_Input.cpp
@@ -2,31 +2,56 @@
 
 void KeyShow(char* Keyinput) {
 	GetHitKeyStateAll(Keyinput);//キーの押下状態を格納。
-	if ( Keyinput[KEY_INPUT_Z] == 1) {	//Zキーが押された時
-		DrawFormatString(0, 0, WhiteColor, "Zキーが押されました。");
+	int showpos = -20;
+	int showpos2 = -20;
+	if (Keyinput[KEY_INPUT_A] == 1) {
+		showpos += 20;
+		DrawFormatString(0, showpos, WhiteColor, "Aキーが押されました。");
+	}
+	if (Keyinput[KEY_INPUT_S] == 1) {
+		showpos += 20;
+		DrawFormatString(0, showpos, WhiteColor, "Sキーが押されました。");
+	}
+	if (Keyinput[KEY_INPUT_Z] == 1) {
+		showpos += 20;
+		DrawFormatString(0, showpos, WhiteColor, "Zキーが押されました。");
 	}
 	if (Keyinput[KEY_INPUT_X] == 1) {
-		DrawFormatString(0, 20, WhiteColor, "Xキーが押されました。");
+		showpos += 20;
+		DrawFormatString(0, showpos, WhiteColor, "Xキーが押されました。");
 	}
 	if (Keyinput[KEY_INPUT_C] == 1) {
-		DrawFormatString(0, 40, WhiteColor, "Cキーが押されました。");
+		showpos += 20;
+		DrawFormatString(0, showpos, WhiteColor, "Cキーが押されました。");
 	}
 	if (Keyinput[KEY_INPUT_LEFT] == 1) {
-		DrawFormatString(0, 60, WhiteColor, "←キーが押されました。");
+		showpos2 += 20;
+		DrawFormatString(600, showpos2, WhiteColor, "←キーが押されました。");
 	}
 	if (Keyinput[KEY_INPUT_RIGHT] == 1) {
-		DrawFormatString(0, 80, WhiteColor, "→キーが押されました。");
+		showpos2 += 20;
+		DrawFormatString(600, showpos2, WhiteColor, "→キーが押されました。");
 	}
 	if (Keyinput[KEY_INPUT_UP] == 1) {
-		DrawFormatString(0, 100, WhiteColor, "↑キーが押されました。");
+		showpos2 += 20;
+		DrawFormatString(600, showpos2, WhiteColor, "↑キーが押されました。");
 	}
 	if (Keyinput[KEY_INPUT_DOWN] == 1) {
-		DrawFormatString(0, 120, WhiteColor, "↓キーが押されました。");
+		showpos2 += 20;
+		DrawFormatString(600, showpos2, WhiteColor, "↓キーが押されました。");
 	}
 	if (Keyinput[KEY_INPUT_SPACE] == 1) {
-		DrawFormatString(0, 140, WhiteColor, "SPACEキーが押されました。");
+		showpos2 += 20;
+		DrawFormatString(600, showpos2, WhiteColor, "SPACEキーが押されました。");
 	}
+	if (Keyinput[KEY_INPUT_LSHIFT] == 1) {
+		showpos2 += 20;
+		DrawFormatString(600, showpos2, WhiteColor, "左シフトキーが押されました。");
+	}
+
 }
 
 //方向キーは３つ同時押しには対応してない。MacbookAir 2012の仕様かもしれないけど。
 //３つ同時に押して操作することはないだろうが一応。
+
+

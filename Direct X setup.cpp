72ffbd4197291bb0d3a5bba54ee6533645a68248
ryo <otyaroo@live.jp>
2015-10-04 17:48:24 +0900

    グローバル変数を消してジャンプ関数追加
    キー押下情報を格納するKeysをInitialize.hへ移動
    Jump関数の導入

--- Direct X setup.cpp
+++ Direct X setup.cpp
@@ -1,22 +1,29 @@
+#include "initialize.h"
 #include "DirectX_Input.h"
+#include "Jump.h"
+
 
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	SetGraphMode(800, 600, 16);
 	ChangeWindowMode(TRUE);//ウィンドウモードで起動
-	char keys[256]; //キーの押下を格納する配列。
 	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
 	{
 		return -1;			// エラーが起きたら直ちに終了
 	}
 	
+
+
 	while (true) {
 		SetDrawScreen(DX_SCREEN_BACK);
-		
 		ClearDrawScreen();
-		KeyShow(keys);
-		DrawPixel(320, 240, GetColor(255, 255, 255));	// 点を打つ
+		
+		KeyShow(Keys);
+		Jump(&JumpPower, &Player_Y, Keys);
+		DrawPixel(Player_X, Player_Y, GetColor(255, 255, 255));	// 点を打つ
+		
+		
 		ScreenFlip();
 	}
 				// キー入力待ち
